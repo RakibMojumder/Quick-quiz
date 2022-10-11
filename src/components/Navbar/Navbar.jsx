f38a4ac99504ed90d40a0d703456f628003fed76
Rakib Ahmed <sujonahmed45a4@gmail.com>
2022-10-11 09:32:04 +0600

    navbar responsive completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,92 +1,196 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../.././assets/img/logo.png";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <header className="bg-gray-200">
-      <div className="container flex justify-between h-16 mx-auto">
+    <div className="px-4 py-3 border-b-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-20 lg:px-8">
+      <div className="relative flex items-center justify-between">
         <a
-          rel="noopener noreferrer"
           href="/"
-          aria-label="Back to homepage"
-          className="flex items-center font-semibold p-2"
+          aria-label="Company"
+          title="Company"
+          className="inline-flex items-center"
         >
-          <img className="h-16" src={logo} alt="" />
-          <h1 className="text-3xl -ml-3 font-bold">uick Quiz</h1>
+          <img className="h-12" src={logo} alt="" />
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+            Quick Quiz
+          </span>
         </a>
-        <ul className="items-stretch hidden space-x-3 md:flex">
-          <li className="flex">
+        <ul className="flex items-center hidden space-x-8 lg:flex">
+          <li>
             <NavLink
-              rel="noopener noreferrer"
               to="/home"
+              aria-label="Home"
               className={({ isActive }) =>
                 isActive
-                  ? "flex text-blue-500 font-semibold items-center px-4 -mb-1 border-b-2 border-transparent"
-                  : "flex items-center font-semibold px-4 -mb-1 border-b-2 border-transparent"
+                  ? "border-b-2 border-gray-700 font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  : "font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               }
             >
               Home
             </NavLink>
           </li>
-          <li className="flex">
+          <li>
             <NavLink
-              rel="noopener noreferrer"
               to="/topics"
+              aria-label="Topics"
               className={({ isActive }) =>
                 isActive
-                  ? "flex text-blue-500 font-semibold items-center px-4 -mb-1 border-b-2 border-transparent"
-                  : "flex items-center font-semibold px-4 -mb-1 border-b-2 border-transparent"
+                  ? "border-b-2 border-gray-700 font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  : "font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               }
             >
               Topics
             </NavLink>
           </li>
-          <li className="flex">
+          <li>
             <NavLink
-              rel="noopener noreferrer"
               to="/statistics"
+              aria-label="Statistics"
               className={({ isActive }) =>
                 isActive
-                  ? "flex text-blue-500 font-semibold items-center px-4 -mb-1 border-b-2 border-transparent"
-                  : "flex items-center font-semibold px-4 -mb-1 border-b-2 border-transparent"
+                  ? "border-b-2 border-gray-700 font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  : "font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               }
             >
               Statistics
             </NavLink>
           </li>
-          <li className="flex">
+          <li>
             <NavLink
-              rel="noopener noreferrer"
               to="/blog"
+              aria-label="Blog"
               className={({ isActive }) =>
                 isActive
-                  ? "flex text-blue-500 font-semibold items-center px-4 -mb-1 border-b-2 border-transparent"
-                  : "flex items-center font-semibold px-4 -mb-1 border-b-2 border-transparent"
+                  ? "border-b-2 border-gray-700 font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  : "font-semibold tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
               }
             >
               Blog
             </NavLink>
           </li>
         </ul>
-        <button className="flex justify-end p-4 md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
+        <div className="lg:hidden">
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            onClick={() => setIsMenuOpen(true)}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
+            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+              />
+            </svg>
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <a
+                      href="/"
+                      aria-label="Company"
+                      title="Company"
+                      className="inline-flex items-center"
+                    >
+                      <svg
+                        className="w-8 text-deep-purple-accent-400"
+                        viewBox="0 0 24 24"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        stroke="currentColor"
+                        fill="none"
+                      >
+                        <rect x="3" y="1" width="7" height="12" />
+                        <rect x="3" y="17" width="7" height="6" />
+                        <rect x="14" y="1" width="7" height="6" />
+                        <rect x="14" y="11" width="7" height="12" />
+                      </svg>
+                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        Company
+                      </span>
+                    </a>
+                  </div>
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <NavLink
+                        to="/"
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Product
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/"
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Features
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/"
+                        aria-label="Product pricing"
+                        title="Product pricing"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Pricing
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/"
+                        aria-label="About us"
+                        title="About us"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        About us
+                      </NavLink>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </header>
+    </div>
   );
 };
 
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -11,14 +11,13 @@ import {
 
 const Statistics = () => {
   const quizTopic = useContext(QuizTopicsContext).data;
-  console.log(quizTopic);
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <h1 className="text-2xl mb-10 font-semibold uppercase">
         Quiz Total <span className="text-blue-500">StatisTics</span>
       </h1>
-      <LineChart width={500} height={400} data={quizTopic}>
+      <LineChart width={400} height={400} data={quizTopic}>
         <Line type="monotone" dataKey="total" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="name" />
